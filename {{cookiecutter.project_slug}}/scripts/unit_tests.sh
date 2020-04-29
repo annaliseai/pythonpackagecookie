05ff5cc088b3351ac3a1029c45a8e4b422c78d30
Aniket Sharma <aniket.sharma@annalise.ai>
2020-04-29 12:49:55 +1000

    bug: removed de-id specific scripts, and called the build script something more generic

--- {{cookiecutter.project_slug}}/scripts/unit_tests.sh
+++ {{cookiecutter.project_slug}}/scripts/unit_tests.sh
@@ -7,7 +7,3 @@ export LANG=en_AU.utf8
 
 /opt/python/cp37-cp37m/bin/pip install -r requirements.txt
 /opt/python/cp37-cp37m/bin/pytest -v
-
-# Confirm the dictionaries
-/opt/python/cp37-cp37m/bin/python utils/generate_dicom_dict.py
-/opt/python/cp37-cp37m/bin/python utils/confirm_dicom_currency.py
