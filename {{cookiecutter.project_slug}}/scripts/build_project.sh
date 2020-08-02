47d8331c41e4644bc0cb35617c67157c2c3bd14a
Stephen Hogg <stephen.hogg@harrison.ai>
2020-08-02 14:47:28 +1000

    Add pip.conf; Update scripts to use pip.conf; set default column width to 120 in pylintrc

--- {{cookiecutter.project_slug}}/scripts/build_project.sh
+++ {{cookiecutter.project_slug}}/scripts/build_project.sh
@@ -8,16 +8,13 @@ export LANG=en_AU.utf8
 # Install zip for artifacts
 yum install -y zip
 
+cp pip.conf /etc/pip.conf
+
 /opt/python/cp37-cp37m/bin/pip install -r requirements.txt
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 
 if [ ${BUILDKITE_BRANCH} == "master" ]
 then
-
-    # Upload to s3
-    /opt/python/cp37-cp37m/bin/pip install --extra-index-url \
-        http://pypi-prod-annalise-ai.s3-website-ap-southeast-2.amazonaws.com/simple/ \
-        --trusted-host pypi-prod-annalise-ai.s3-website-ap-southeast-2.amazonaws.com s3pkgup
 
     /opt/python/cp37-cp37m/bin/s3pkgup --project {{ cookiecutter.project_slug.replace('_', '-') }}
 
