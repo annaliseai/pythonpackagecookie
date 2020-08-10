a99af389bcd524ef2f2e004308d93cb27a620eec
StephenHogg <StephenHogg@users.noreply.github.com>
2020-08-10 18:42:29 +1000

    Add pip.conf; Update scripts to use pip.conf; set default column widt… (#6)
    * Add pip.conf; Update scripts to use pip.conf; set default column width to 120 in pylintrc
    
    * Update pip.conf
    
    Co-authored-by: Stephen Hogg <stephen.hogg@harrison.ai>

--- {{cookiecutter.project_slug}}/scripts/unit_tests.sh
+++ {{cookiecutter.project_slug}}/scripts/unit_tests.sh
@@ -5,5 +5,7 @@ set -eo
 N_WORKERS=${N_TESTING_WORKERS:-1}
 export LANG=en_AU.utf8
 
+cp pip.conf /etc/pip.conf
+
 /opt/python/cp37-cp37m/bin/pip install -r requirements.txt
 /opt/python/cp37-cp37m/bin/pytest -n=${N_WORKERS} -v
