6575c80355cad9d9f2afe0715781cffed9d4b39e
Ben Johnston <ben.johnston@annalise.ai>
2020-10-22 16:54:27 +1100

    Update to use docker-compose

--- {{cookiecutter.project_slug}}/scripts/build_project.sh
+++ {{cookiecutter.project_slug}}/scripts/build_project.sh
@@ -13,9 +13,15 @@ cp pip.conf /etc/pip.conf
 /opt/python/cp37-cp37m/bin/pip install -r requirements.txt
 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 
-if [ ${BUILDKITE_BRANCH} == "master" ]
+# Upload the package to s3 if on dev or master and only if a tag is assigned
+# as we are using versioneer to manage python build versions
+# We use [semantic versioning](https://semver.org/)
+# For master branch versions use the format x.y.z
+# For develop branch versions use the format x.y.z.devA
+if [[ ${BUILDKITE_TAG} != "" ]]
 then
 
+    # Upload to s3
+    /opt/python/cp37-cp37m/bin/pip install s3pkgup
     /opt/python/cp37-cp37m/bin/s3pkgup --project {{ cookiecutter.project_slug.replace('_', '-') }}
-
 fi
